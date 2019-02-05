df99aefbdf3d37746e6132cdf7e722ed94fe347d
Shayan Memari <memshayan@gmail.com>
2019-02-06 09:01:53 +1100

    refine a rule and readme file

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# auth0-rules
-Some useful Rules in Auth0
+# Some useful Auth0 Rules
+[Rules](https://auth0.com/docs/rules) are code snippets written in JavaScript that are executed as part of the authentication pipeline in [Auth0](https://www.auth0.com/). This happens every time a user authenticates to an application. Rules enable very powerful customizations and extensions to be easily added to Auth0.
+
+### Supported Node runtime
+At this time, both Node 4 and Node 8 are supported to run your Webtask code, however Node 4 is going out of [Long-term support](https://github.com/nodejs/Release#release-schedule). Mingration guide [here](https://auth0.com/docs/migrations/guides/extensibility-node8).
+
+### Avaiable Modules in Rules
+* Webtask Modules: https://auth0-extensions.github.io/canirequire/
+* Additional Modules: https://auth0.com/docs/appliance/modules
+
+### Info for beginners
+* Rules are executed after user authentication so they are part of the authorization process.
+* Rules are run in order, so logically ordering your rules is important.
+* Read Auth0 [best practices](https://auth0.com/docs/best-practices/rules) on Rules.
+
--- rejectDatabaseSignupOrLogin.js
+++ rejectDatabaseSignupOrLogin.js
@@ -1,14 +1,22 @@
+/*
+This rule is used to detect Username-Password Database connection login/signups and reject them if an existing Social account (Google in this case) is found in Auth0.
+This rule relies on the helperFunctions.js rule as well which needs to be defined and ordered earlier than this rule.
+*/
+
 function (user, context, callback) {
 
+  const CONNECTION_NAME = "Username-Password-Authentication";
+  const SOCIAL_CONNECTION_NAME = "google-oauth2";
+
   // Only run this rule when this database connection is used
-  if (context.connection !== "Username-Password-Authentication") {
-    console.log(`Skipping the rejectDatabaseSignupOrLogin Rule: Not a DB signup/login: ${context.connection}`);
+  if (context.connection !== CONNECTION_NAME) {
+    console.log(`Skipping the rejectDatabaseSignupOrLogin Rule: Not a Database signup/login: ${context.connection}`);
     return callback(null, user, context);
   }
 
   // DO NOT run this rule if user logged in without an email.
   if (!user.email) {
-    console.log('Skipping the Social Detection Signup rule: No email');
+    console.log('Skipping the rejectDatabaseSignupOrLogin rule: No email found');
     return callback(null, user, context);
   }
 
@@ -20,7 +28,7 @@ function (user, context, callback) {
         let existingUser = JSON.parse(response);
         let existingUserProvider = existingUser[0].identities[0].provider;
 
-        if (existingUser[0].user_id && existingUser[0].user_id !== null && existingUserProvider === "google-oauth2") {
+        if (existingUser[0].user_id && existingUser[0].user_id !== null && existingUserProvider === SOCIAL_CONNECTION_NAME) {
           console.log(`Existing User provider is: ${existingUserProvider} and User has an existing social connection user_id: ${existingUser[0].user_id}`);
           callback(new UnauthorizedError('You already have a Google social account, please use this to login.'));
         }
@@ -31,7 +39,7 @@ function (user, context, callback) {
       });
 
   } catch (e) {
-    console.log("Exception thrown: " + e);
+    console.log("Exception: " + e);
     throw e;
   }
 }
