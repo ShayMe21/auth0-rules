2889557a21c1df4940878182ae5e39c725dec537
Shayan Memari <memshayan@gmail.com>
2019-02-18 16:23:43 +1100

    api2 rule moved to helper function

--- getManagementApiToken.js
+++ getManagementApiToken.js
@@ -1,33 +1,12 @@
 function getManagementApiToken(user, context, callback) {
   console.log("MANAGEMENT-API RULE RUNNING");
 
-  // A M2M Application authorized to Management APIv2
-  const AUTH0_DOMAIN = 'shayan-dev.au.auth0.com';
-  const CLIENT_ID = '43BKVc57RgIiVJrbM8gRDIBQgofdByYe';
-
   if (!global.requestLib) {
     global.requestLib = require('request-promise');
   }
 
-  // Client credentials grant: https://auth0.com/docs/api/authentication#machine-to-machine-m2m-flow
-  var options = {
-    method: 'POST',
-    url: `https://${AUTH0_DOMAIN}/oauth/token`,
-    headers: {
-      'content-type': 'application/json'
-    },
-    body: {
-      grant_type: 'client_credentials',
-      client_id: CLIENT_ID,
-      client_secret: configuration.client_secret,
-      audience: `https://${AUTH0_DOMAIN}/api/v2/`
-    },
-    json: true,
-    timeout: 2000 // https://github.com/request/request#timeouts
-  };
-
   try {
-    global.requestLib(options).then((response) => {
+    global.getApi2Token().then((response) => {
       console.log("PRINTING API2 Access Token...");
       var api2AccessToken = response.access_token;
       console.log(api2AccessToken);
--- helperFunctions.js
+++ helperFunctions.js
@@ -1,7 +1,7 @@
 function setupCommonFunctions(user, context, callback) {
   const auth0BaseUrl = auth0.baseUrl;
-  
-// https://github.com/request/request-promise
+
+  // https://github.com/request/request-promise
   if (!global.requestLib) {
     global.requestLib = require('request-promise');
   }
@@ -13,7 +13,7 @@ function setupCommonFunctions(user, context, callback) {
 
   if (!global.getUserByEmail) {
     global.getUserByEmail = (email, fields) => {
-      
+
       // Check if user already exists in Auth0
       return global.requestLib.get({
         url: `${auth0BaseUrl}/users-by-email?fields=${fields.join(',')}`,
@@ -32,12 +32,36 @@ function setupCommonFunctions(user, context, callback) {
       if (!start) {
         return '';
       }
-      const precision = 3;  // 3 decimal places
-      const elapsed = process.hrtime(start)[1] / 1000000;   // divide by a million to get ms
+      const precision = 3; // 3 decimal places
+      const elapsed = process.hrtime(start)[1] / 1000000; // divide by a million to get ms
       return elapsed.toFixed(precision);
     };
   }
 
-  callback(null, user, context);
+  if (!global.getApi2Token) {
+    // A M2M Application authorized to Management APIv2
+    const AUTH0_DOMAIN = 'shayan-dev.au.auth0.com';
+    const CLIENT_ID = '43BKVc57RgIiVJrbM8gRDIBQgofdByYe';
 
+    // Client credentials grant: https://auth0.com/docs/api/authentication#machine-to-machine-m2m-flow
+    global.getApi2Token = () => {
+      return global.requestLib({
+        method: 'POST',
+        url: `https://${AUTH0_DOMAIN}/oauth/token`,
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: {
+          grant_type: 'client_credentials',
+          client_id: CLIENT_ID,
+          client_secret: configuration.client_secret,
+          audience: `https://${AUTH0_DOMAIN}/api/v2/`
+        },
+        json: true,
+        timeout: 2000 // https://github.com/request/request#timeouts  
+      });
+    };
+  }
+
+  callback(null, user, context);
 }
