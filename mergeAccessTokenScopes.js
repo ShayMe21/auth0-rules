aed5b0b56b78137f7a1d5f6f4aa586952e049aed
Shayan Memari <memshayan@gmail.com>
2019-03-01 17:58:04 +1100

    refined mergeScopes rule

--- mergeAccessTokenScopes.js
+++ mergeAccessTokenScopes.js
@@ -4,17 +4,22 @@
 */
 
 function mergeAccessTokenScopes(user, context, callback) {
-  let existingScopes = context.request.body.scope ? context.request.body.scope : '';
-  var mergedScopes;
-  const MY_CLIENT_ID = '123';   // Change this to you own CLIENT ID
+  const MY_CLIENT_ID = '123';
   
-  console.log(`Existing Scopes sent in the request: ${existingScopes}`);
-  
-  if (context.clientID === MY_CLIENT_ID){
-    console.log(`merging scopes for clientID ${MY_CLIENT_ID}`);
-    mergedScopes = existingScopes.concat([" offline_access"]);
+  if (context.clientID !== MY_CLIENT_ID){
+    console.log(`Skipping RULE mergeAccessTokenScopes`);
+    callback(null, user, context);
   }
   
+  let existingScopes = context.request.body.scope ? context.request.body.scope : '';
+  var mergedScopes;
+  
+  console.log(`Existing Scopes sent in the request: ${existingScopes}`);
+
+  console.log(`merging scopes for clientID ${MY_CLIENT_ID}`);
+  
+  mergedScopes = existingScopes + " offline_access"; // Concat any extra scopes
+
   console.log(`Merged Scopes: ${mergedScopes}`);
   
   context.accessToken.scope = mergedScopes;
